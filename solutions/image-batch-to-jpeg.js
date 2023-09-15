682b047ab6bd03759ef5815959aec0cbf847832d
Daniel Graciliano Pereira <87447363+DanielGPereira@users.noreply.github.com>
2023-09-15 00:10:22 -0300

    Add image_batch-to-jpeg

---
+++ solutions/image-batch-to-jpeg.js
@@ -0,0 +1,35 @@
+
+
+const sharp = require('sharp');
+const fs = require('fs');
+const path = require('path');
+const { mkdirp } = require('mkdirp');
+
+const dirInIndex = process.argv.indexOf('--dir_in');
+const dirOutIndex = process.argv.indexOf('--dir_out');
+
+const dirInValue = process.argv[dirInIndex + 1];
+
+let dirOutValue = dirOutIndex > 0 ? process.argv[dirOutIndex + 1] : dirInValue;
+
+dirOutValue = path.join(dirOutValue, 'jpeg-images');
+
+fs.mkdirSync(dirOutValue, { recursive: true })
+
+
+fs.readdir(dirInValue, (err, rawFiles) => {
+    rawFiles.forEach( file => {
+
+        if(file.split('.').length > 1)
+        {
+            sharp(dirInValue + '\\' + file).jpeg({ mozjpeg: true })
+            .toBuffer()
+            .then(imageBuffer => fs.writeFile(path.join(dirOutValue, file.split('.')[0] + '.jpg') , imageBuffer, () => {}))
+        }
+    })
+})
+
+
+
+
+
